10fcfe44876a387ec38b06458813e79015e83804
nufa_sempai <nufa_sempai@mail.ru>
2023-01-31 23:41:14 +0300

    ДЗ 11 - методы выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,62 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int year = 2020;
+        printInfoAboutYear(year);
+        printInfoAboutVersion(0, 2023);
+        int deliveryDistance = 18;
+        int days = calculateDeliveryDays(deliveryDistance);
+        if (days == -1) {
+            System.out.println("Доставка не возможна на расстоянии " + deliveryDistance + " км");
+        } else {
+            System.out.println("Время доставки " + days + " дн.");
+        }
     }
-}
+
+    private static int calculateDeliveryDays(int deliveryDistance) {
+        if (deliveryDistance >= 0 && deliveryDistance <= 20) {
+            return 1;
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            return 2;
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            return 2;
+        } else {
+            return -1;
+        }
+    }
+
+    private static String getInfoAboutVersion(int os, int productionYear) {
+        int currentYear = LocalDate.now().getYear();
+        StringBuilder result = new StringBuilder("Установите ");
+        if (productionYear < currentYear) {
+            result.append("облегченная версия для ");
+        } else {
+            result.append("версия для ");
+        }
+        if (os == 0) {
+            result.append("iOS");
+        } else {
+            result.append("Android");
+        }
+        return result.toString();
+    }
+
+    private static void printInfoAboutYear(int year) {
+        boolean leapYear = isLeapYear(year);
+        if (leapYear) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год НЕ является високосным");
+        }
+    }
+
+    private static void printInfoAboutVersion(int os, int productionYear) {
+        System.out.println(getInfoAboutVersion(os, productionYear));
+    }
+
+    private static boolean isLeapYear(int year) {
+        return year % 4 == 0 && year % 100 !=0 || year % 400 == 0;
+    }
+
+    }
